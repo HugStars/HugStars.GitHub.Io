39ad8c68730c7bd12e9ec5af6df5ed4f88f3ca55
HugStars <76810475+HugStars@users.noreply.github.com>
2021-02-27 05:50:56 -0600

    Add files via upload

---
+++ javascript/addbaidu.js
@@ -0,0 +1,29 @@
+let site = window.location.href;
+site = site.split(/git/i)[1].split('/')[0].toLowerCase();
+console.log(site);
+
+let xhr = new XMLHttpRequest();
+xhr.open("GET", `https://hugstars.git${site}.io/data/baidu.json`);
+xhr.send();
+xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+        callBack(JSON.parse(xhr.responseText));
+    }
+}
+
+function callBack(data) {
+    console.log(data);
+    if (site == 'hub') {
+        site = data.github;
+    } else {
+        site = data.gitee;
+    }
+
+    (function () {
+        var hm = document.createElement("script");
+        hm.src = site;
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(hm, s);
+    })();
+
+}
