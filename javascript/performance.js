240cec19cdc14a1928997974831bd0b04e73a149
HugStars <hugstars@vip.qq.com>
2022-07-01 15:36:09 +0800

    修改log的性能样式

--- javascript/performance.js
+++ javascript/performance.js
@@ -1,17 +1,26 @@
 window.addEventListener('load', function () {
 	let { connectEnd, connectStart, domContentLoadedEventEnd, domInteractive, domLoading, domainLookupEnd, domainLookupStart, fetchStart, loadEventStart, requestStart, responseEnd, responseStart } = window.performance.timing;
-	let style = 'color:green;font-weight:900';
-	console.log("%c DNS解析耗时:", style, domainLookupEnd - domainLookupStart);
-	console.log("%c TCP连接耗时:", style, connectEnd - connectStart);
-	console.log("%c 网络请求耗时(TTFB):", style, responseStart - requestStart);
-	console.log("%c 数据传输耗时:", style, responseEnd - responseStart);
-	console.log("%c DOM解析耗时:", style, domInteractive - responseEnd);
-	console.log("%c 资源加载耗时:", style, loadEventStart - domContentLoadedEventEnd);
-	console.log("%c 首包时间:", style, responseStart - domainLookupStart);
-	console.log("%c 首次渲染时间/白屏时间:", style, domLoading - fetchStart);
-	console.log("%c 首次可交互时间:", style, domInteractive - fetchStart);
-	console.log("%c DOM Ready时间:", style, domContentLoadedEventEnd - fetchStart);
-	console.log("%c 页面完全加载时间:", style, loadEventStart - fetchStart)
+	let style1 = 'color:white;background:#fb8b05;font-weight:900;padding:2px 10px;border-radius: 5px 0 0 5px;';
+	let style2 = 'color:white;background:#41CF7D;font-weight:900;padding:2px 10px;border-radius: 0 5px 5px 0;';
+
+	let obj = {
+		"DNS解析": domainLookupEnd - domainLookupStart,
+		"TCP连接": connectEnd - connectStart,
+		"网络请求(TTFB)": responseStart - requestStart,
+		"数据传输": responseEnd - responseStart,
+		"DOM解析": domInteractive - responseEnd,
+		"资源加载": loadEventStart - domContentLoadedEventEnd,
+		"首包时间": responseStart - domainLookupStart,
+		"首次渲染时间/白屏时间": domLoading - fetchStart,
+		"首次可交互时间": domInteractive - fetchStart,
+		"DOM Ready时间": domContentLoadedEventEnd - fetchStart,
+		"首次可交互时间": domInteractive - fetchStart,
+		"页面完全加载时间": loadEventStart - fetchStart
+	}
+
+	for (const key in obj) {
+		console.log(`%c${key}%c${obj[key]} ms`, style1, style2)
+	}
 
 	if (location.protocol != 'file:') {
 		navigator.serviceWorker.register('./javascript/serviceWorker.js');
